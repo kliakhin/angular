2dec374da6a026db13b7101265399353f987aaa2
Yevgen.Kliakhin <kliakhin.e.a@gmail.com>
2017-02-02 21:09:02 +0200

    init

--- app/testController.js
+++ app/testController.js
@@ -6,6 +6,15 @@ function testCtrl($scope) {
 
     $scope.title = 'Hello world!';
 
+    $scope.$watch('flag', function (newVal, oldVal) {
+
+    });
+
+    $scope.clickHandler = function () {
+        console.log($scope.title);
+    }
+
 }
+
 
 angular.module('app').controller('testCtrl', testCtrl);
